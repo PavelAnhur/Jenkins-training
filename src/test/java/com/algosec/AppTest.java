fc45a02b230fe67aaa426f41882b7988ded3e40d
Pavel Anhur <pavelanhur@gmail.com>
2024-06-10 13:08:29 +0300

    tests

--- src/test/java/com/algosec/AppTest.java
+++ src/test/java/com/algosec/AppTest.java
@@ -8,10 +8,12 @@ public class AppTest {
     @Test
     public void add() {
         Assert.assertEquals(5, new Main().add(3, 2));
+        Assert.assertEquals(10, new Main().add(7, 3));
     }
 
     @Test
     public void subtract() {
         Assert.assertEquals(1, new Main().subtract(3, 2));
+        Assert.assertEquals(6, new Main().subtract(12, 6));
     }
 }
